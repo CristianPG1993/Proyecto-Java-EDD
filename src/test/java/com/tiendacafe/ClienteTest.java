3e9ffa40d51f51440ad6298936c3953857d7b5a2
CristianPG1993 <cristian.panos.gallego@gmail.com>
2025-07-08 13:24:59 +0200

    ✅ Implementada clase Cliente con test y Javadoc

--- src/test/java/com/tiendacafe/ClienteTest.java
+++ src/test/java/com/tiendacafe/ClienteTest.java
@@ -1,4 +1,40 @@
 package com.tiendacafe;
 
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ClienteTest {
+
+    private Cliente cliente;
+
+    @BeforeEach
+    void prepararCliente(){
+        cliente = new Cliente("Laura", "09034502V",
+                "612345678", "laura@email.com");
+    }
+
+    @Test
+    public void testGetNombre(){
+
+        assertEquals("Laura", cliente.getNombre());
+
+    }
+
+    @Test
+    public void testGetDni(){
+
+        assertEquals("09034502V", cliente.getDni());
+    }
+
+    @Test
+    public void testGetTelefono(){
+
+        assertEquals("612345678", cliente.getTelefono());
+    }
+
+    @Test
+    public void testGetEmail(){
+
+        assertEquals("laura@email.com", cliente.getEmail());
+    }
 }
