9d56719c69d4eb308af28c75a2b53e89592b6721
CristianPG1993 <cristian.panos.gallego@gmail.com>
2025-07-16 16:33:15 +0200

    Añadir submenú de gestión de cafés y cambios en los métodos de GestorCafés

--- src/main/java/com/tiendacafe/GestorCafes.java
+++ src/main/java/com/tiendacafe/GestorCafes.java
@@ -34,7 +34,7 @@ public class GestorCafes {
      */
 
 
-    public static void darAltaCafe(){
+    public void darAltaCafe(){
         Scanner scanner = new Scanner(System.in);
         scanner.useLocale(Locale.US);
 
@@ -75,7 +75,7 @@ public class GestorCafes {
      * La lista original no se modifica; se crea una copia temporal para mostrar los resultados.
      */
 
-    public static void listarCafes(){
+    public void listarCafes(){
 
         Scanner scanner = new Scanner(System.in);
         int opcion = 0;
@@ -129,7 +129,7 @@ public class GestorCafes {
      * Utiliza la entrada por consola para capturar la variedad a buscar.
      */
 
-    public static void buscarCafePorVariedad(){
+    public void buscarCafePorVariedad(){
 
         Scanner scanner = new Scanner(System.in);
 
