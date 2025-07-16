56eb6f0c69174bbeb071b07142a701e69ef9ca95
CristianPG1993 <cristian.panos.gallego@gmail.com>
2025-07-16 16:15:04 +0200

    Corregir cambios en los test de café

--- src/test/java/com/tiendacafe/VentaTest.java
+++ src/test/java/com/tiendacafe/VentaTest.java
@@ -29,9 +29,9 @@ public class VentaTest {
                 "612345678", "laura@email.com");
 
         cafe1 = new Cafe("Don Pedro", "Caturra",
-                "Guatemala", 10.50);
+                "Guatemala", 10.50, 4);
         cafe2 = new Cafe("Monte Azul", "Bourbon",
-                "El Salvador", 12);
+                "El Salvador", 12, 5);
 
         listaCafes = new ArrayList<>();
         listaCafes.add(cafe1);
@@ -81,7 +81,7 @@ public class VentaTest {
     public void testAddCafe(){
 
         Cafe nuevoCafe = new Cafe("Geisha", "tostado",
-                "Japón", 15);
+                "Japón", 15, 6);
 
         venta.addCafe(nuevoCafe);
 
